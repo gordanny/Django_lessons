e388256e6720fcc7fb306c3cf9df4fc4596d897b
Denis Gorshkov <gordanny.social@gmail.com>
2021-06-15 18:27:14 +0500

    Added views and html-templates for home,topics and enties pages. Also added base.html template from which templates for pages are inherited.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page.
     path('', views.index, name='index'),
+    # List of topics.
+    path('topics/', views.topics, name='topics'),
+    # List of entries for topics.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
-    """Home page of Learning Log app"""
+    """Home page of Learning Log app."""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Page with list of all learning topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Page with list of entries about the topic."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
